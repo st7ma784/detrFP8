db13217f76f5b3272d024ba5e9008b641e0cdca9
steve <you@example.com>
2024-05-02 00:32:54 +0100

    add hist plot!

--- models/matcher.py
+++ models/matcher.py
@@ -7,7 +7,7 @@ from scipy.optimize import linear_sum_assignment
 from torch import nn
 
 from util.box_ops import box_cxcywh_to_xyxy, generalized_box_iou
-
+import matplotlib.pyplot as plt
 
 class HungarianMatcher(nn.Module):
     """This class computes an assignment between the targets and the predictions of the network
@@ -30,7 +30,7 @@ class HungarianMatcher(nn.Module):
         self.cost_bbox = cost_bbox
         self.cost_giou = cost_giou
         assert cost_class != 0 or cost_bbox != 0 or cost_giou != 0, "all costs cant be 0"
-
+        self.counter=0
     @torch.no_grad()
     def forward(self, outputs, targets):
         """ Performs the matching
@@ -76,7 +76,11 @@ class HungarianMatcher(nn.Module):
         # Final cost matrix
         C = self.cost_bbox * cost_bbox + self.cost_class * cost_class + self.cost_giou * cost_giou
         C = C.view(bs, num_queries, -1).cpu()
-
+        self.counter+=1
+        if self.counter%1000==0:
+            plt.hist(C.flatten().numpy(),bins=100,label=self.counter)
+            plt.title("Histogram at {}".format(self.counter))
+            plt.savefig("Histogram at {}.png".format(self.counter))
         sizes = [len(v["boxes"]) for v in targets]
         indices = [linear_sum_assignment(c[i]) for i, c in enumerate(C.split(sizes, -1))]
         return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
