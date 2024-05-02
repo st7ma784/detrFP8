972af6f08955df1f83d7ab915a9f4a4bff09c041
steve <you@example.com>
2024-05-02 23:50:12 +0000

    add final experiment

--- models/matcher.py
+++ models/matcher.py
@@ -78,28 +78,81 @@ class HungarianMatcher(nn.Module):
 
         # Final cost matrix
         C = self.cost_bbox * cost_bbox + self.cost_class * cost_class + self.cost_giou * cost_giou
-        C = C.view(bs, num_queries, -1).cpu() #Dist=N(6,3)  Ideal is 1-|N(0,0.2)|
+        C = C.view(bs, num_queries, -1) #Dist=N(6,3)  Ideal is 1-|N(0,0.2)|
         self.counter+=1
-        if self.counter%1000==0:
-            plt.hist(C.flatten().numpy(),bins=100,label=self.counter)
-            plt.title("Histogram at {}".format(self.counter))
-            plt.savefig("Histogram at {}.png".format(self.counter))
-        sizes = [len(v["boxes"]) for v in targets]
-        indices = [linear_sum_assignment(c[i],maximize=False) for i, c in enumerate(C.split(sizes, -1))]#C.shape is... 
+        # if self.counter%1000==0:
+        #     plt.hist(C.flatten().numpy(),bins=100,label=self.counter)
+        #     plt.title("Histogram at {}".format(self.counter))
+        #     plt.savefig("Histogram at {}.png".format(self.counter))
+        
         #optimize C for FP8
-        print("C Shape is {}".format(C.shape))
-        print("sizes is {}".format(sizes))
+        #print("C Shape is {}".format(C.shape))   B,100,Annotations
+        #print("sizes is {}".format(sizes)) # Sizes is B numbers that sum to A 
 
-        print("output is {}".format(indices))
-        C=-C 
+        #print("output is {}".format(indices)) (B * ([0-100]*s, range(0,s)))
+        sizes = [len(v["boxes"]) for v in targets]
+        #indices = [linear_sum_assignment(c[i],maximize=False) for i, c in enumerate(C.cpu().split(sizes, -1))]#C.shape is... 
+
+
+        '''
+        Tests,
+        Just use argmin as is,  #3:39/epoch
+        '''
+        # E=torch.argmin(C,dim=1) #B,A 
+        # indexes=[torch.arange(s) for s in sizes]
+        # #print(C.split(sizes,-1))
+        # locations=E[torch.diag(torch.ones(E.shape[0],dtype=torch.bool)).repeat_interleave(torch.tensor(sizes,dtype=torch.long),dim=1)].split(sizes)
+        # indices=list(zip(locations,indexes))
+        # print(indices)
+
+
+        '''
+        Use argmax after FP8...
+        
+        
+        '''
+
+        # prob=self.m.cdf(-C) # using - so that the minimize LSA can become a maximize function!
+        # reDistC=self.tgt.icdf(prob)
+
+        # C=torch.argmax(reDistC,dim=1) #B,A 
+        # indexes=[torch.arange(s) for s in sizes]
+        # #print(C.split(sizes,-1))
+        # locations=C[torch.diag(torch.ones(C.shape[0],dtype=torch.bool)).repeat_interleave(torch.tensor(sizes,dtype=torch.long),dim=1)].split(sizes)
+        # indices=list(zip(locations,indexes))
+        # # print(indices)
+
+        '''
+        
+        use stepped argmax as per my_function
+        '''
+        S=torch.tensor(sizes,dtype=torch.long,device=C.device)
         prob=self.m.cdf(-C) # using - so that the minimize LSA can become a maximize function!
-        reDistC=self.tgt.icdf(prob)
-
-
-
-
-        return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
+        fx=self.tgt.icdf(prob).permute(0,2,1)[torch.diag(torch.ones_like(S,dtype=torch.bool,device=C.device)).repeat_interleave(S,dim=1)]
+        row_lookups=lookup_sizes_vfast(S)
+        outputs=Batch_MyLinearSumAssignment(fx.T,row_lookups=row_lookups)
+        indices=[torch.nonzero(o,as_tuple=True) for o in outputs.split(sizes,dim=1)]
+        return indices#[(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64)) for i, j in indices]
 
 
 def build_matcher(args):
     return HungarianMatcher(cost_class=args.set_cost_class, cost_bbox=args.set_cost_bbox, cost_giou=args.set_cost_giou)
+
+def Batch_MyLinearSumAssignment(Batched_TruthTensor:torch.Tensor,row_lookups:torch.Tensor, maximize=True,lookahead=2):
+    # results=torch.zeros_like(Batched_TruthTensor,dtype=torch.bool)    
+    for _ in range(row_lookups.shape[-1]): # number of columns 
+        deltas=torch.diff(torch.topk(torch.clamp(Batched_TruthTensor,max=100),lookahead,dim=0,largest=maximize).values,n=lookahead-1,dim=0)
+        col_index=torch.argmax(torch.abs(deltas[0])) 
+        row_index=torch.argmax(Batched_TruthTensor[:,col_index],dim=0) # BxB
+        Batched_TruthTensor[:,col_index]=0
+        Batched_TruthTensor[row_index,row_lookups[col_index]]=0
+        Batched_TruthTensor[row_index,col_index]=-1e-7
+    return Batched_TruthTensor<0
+
+
+@torch.no_grad()
+def lookup_sizes_vfast(sizes:torch.Tensor):
+    return torch.diag(torch.ones_like(sizes)).repeat_interleave(sizes,dim=0).repeat_interleave(sizes,dim=1).to(dtype=torch.long,device=sizes.device)
+
+
+
