118e4d284d39dca8940a0768cb2c3f2edd7c7ae7
John Doe <johndoe@example.com>
2024-05-09 11:10:05 +0100

    fx

--- savescript.py
+++ savescript.py
@@ -6,7 +6,7 @@ for root, dirs, files in os.walk('.'):
     if not files:
         continue
     for file in files: 
-        if file.endswith('.py'):
+        if file.endswith('.py') or file.endswith(".yml"):
             os.makedirs(os.path.join("codeForAssistant",*root.split("\\")[:-1]), exist_ok=True)
             try:
                 shutil.copy(os.path.join(root, file), os.path.join('codeForAssistant',*root.split("\\")[:-1], file.split("\\")[-1].split(".")[0] + '.txt'))
