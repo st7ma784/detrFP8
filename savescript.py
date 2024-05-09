9783aa1b876cca3376d2cf8e282d3b91b2ce3ff5
John Doe <johndoe@example.com>
2024-05-09 11:25:27 +0100

    fx

--- savescript.py
+++ savescript.py
@@ -1,12 +1,12 @@
 import os
 import shutil
-
+endings=[".py",".yml","Dockerfile",".md"]
 os.makedirs('codeForAssistant', exist_ok=True)
 for root, dirs, files in os.walk('.'):
     if not files:
         continue
     for file in files: 
-        if file.endswith('.py') or file.endswith(".yml"):
+        if any([file.endswith(a) for a in endings]):
             os.makedirs(os.path.join("codeForAssistant",*file.split("\\")[:-1]), exist_ok=True)
             try:
                 shutil.copy(os.path.join(root, file), os.path.join('codeForAssistant',*file.split("\\")[:-1], file.split("\\")[-1].split(".")[0] + '.txt'))
