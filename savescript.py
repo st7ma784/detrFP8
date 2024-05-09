ec213c5cc0bbd8ef164cd4d5a8345836fb598b57
John Doe <johndoe@example.com>
2024-05-09 11:08:15 +0100

    fx

---
+++ savescript.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+os.makedirs('codeForAssistant', exist_ok=True)
+for root, dirs, files in os.walk('.'):
+    if not files:
+        continue
+    for file in files: 
+        if file.endswith('.py'):
+            os.makedirs(os.path.join("codeForAssistant",*root.split("\\")[:-1]), exist_ok=True)
+            try:
+                shutil.copy(os.path.join(root, file), os.path.join('codeForAssistant',*root.split("\\")[:-1], file.split("\\")[-1].split(".")[0] + '.txt'))
+            except Exception as e:
+                print(e)
